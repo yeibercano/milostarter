a8d83bd7c3ef283df97445917bb9cf5c8fcc66d9
Yeiber Cano <cano@adobe.com>
2023-06-19 15:28:29 -0700

    remove double code

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -4,17 +4,6 @@
 
 import { getLibs } from '../../scripts/utils.js';
 
-export default async function init(el) {
-  const { textContent } = el;
-  el.innerHTML = '';
-  const textArr = textContent.trim().split(',');
-  const name = textArr[1] || 'World';
-  const hello = textArr[0] || 'Hello';
-  const { createTag } = await import(`${getLibs()}/utils/utils.js`);
-  const helloEl = createTag('h2', { class: 'hello-title'}, `${hello},`);
-  const nameEl = createTag('p', { class: 'hello-name' }, name);
-  el.append(helloEl, nameEl);
-}
 
 export default async function init(el) {
   const { textContent } = el;
